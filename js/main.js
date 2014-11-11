a3586a6b8d2510a084030e87b956aa4d221c8f20
vinhub <vin.bhalerao@gmail.com>
2014-11-10 16:15:45 -0800

    bug fixes to pagination.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ app.factory('Backend', ['$http',
 
         return {
             featured: get('data/featured.json'),
-            projects: get('https://api.github.com/orgs/microsoft/repos?per_page=100'),
+            projects: get('data/allprojects.json' /* 'https://api.github.com/orgs/microsoft/repos?per_page=100' */),
             orgs: get('data/organization.json')
         }
     }
@@ -25,8 +25,7 @@ app.factory('Backend', ['$http',
         });
 
         Backend.projects().then(function(data) {
-            self.projects = data;
-            
+            self.projects = data.AllProjects;
             $scope.currentPage = 1; //current page
             $scope.maxSize = 5; //pagination max size
             $scope.entryLimit = 12; //max rows for data table
@@ -48,7 +47,7 @@ app.factory('Backend', ['$http',
             self.featured.forEach(function (name) {
                 for (var i = 0; i < self.projects.length; i++) {
                     var project = self.projects[i];
-                    if (project.name == name) {
+                    if (project.Name == name) {
                         self.featuredProjects.push(project);
                         return;
                     }
@@ -56,9 +55,8 @@ app.factory('Backend', ['$http',
              });
         });
     }
-]);
-    
-app.filter('startFrom', function() {
+])
+.filter('startFrom', function() {
     return function(input, start) {
         if (input) {
             start = +start; //parse to int
